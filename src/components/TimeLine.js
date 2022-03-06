7b1c005836cc4ef34811201cf339fb916232caa1
uberguri <bangdy14@gmail.com>
2022-03-06 19:30:25 +0900

    TimeLine, refactoring

--- src/components/TimeLine.js
+++ src/components/TimeLine.js
@@ -10,76 +10,36 @@ import date from "date-and-time";
 
 const timeFormat = "HH:mm A";
 
+const TimeItem = (props) => {
+  const { time, k } = props;
+
+  return (
+    <TimelineItem>
+      <TimelineOppositeContent color="text.secondary">
+        {date.format(new Date(time[k]), timeFormat)}
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        <TimelineDot />
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>{k.includes("Start") ? "시작" : "종료"}</TimelineContent>
+    </TimelineItem>
+  );
+};
+
 const TimeLine = (props) => {
   const { time } = props;
+
+  const validTimes = Object.keys(time)
+    .filter((k) => k[0] === "t" && time[k])
+    .sort((i, j) => (time[i] > time[j] ? 1 : -1));
 
   return (
     <React.Fragment>
       <Timeline position="alternate">
-        <TimelineItem>
-          <TimelineOppositeContent color="text.secondary">
-            {date.format(new Date(time.t1Start), timeFormat)}
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>시작</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent color="text.secondary">
-            {date.format(new Date(time.t1Finish), timeFormat)}
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>종료</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent color="text.secondary">
-            {date.format(new Date(time.t2Start), timeFormat)}
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>시작</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent color="text.secondary">
-            {date.format(new Date(time.t2Finish), timeFormat)}
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>종료</TimelineContent>
-        </TimelineItem>
-        {time.t3Start ?? (
-          <TimelineItem>
-            <TimelineOppositeContent color="text.secondary">
-              {date.format(new Date(props.time.t3Start), timeFormat)}
-            </TimelineOppositeContent>
-            <TimelineSeparator>
-              <TimelineDot />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>시작</TimelineContent>
-          </TimelineItem>
-        )}
-        {time.t3Start ?? (
-          <TimelineItem>
-            <TimelineOppositeContent color="text.secondary">
-              {date.format(new Date(props.time.t3Finish), timeFormat)}
-            </TimelineOppositeContent>
-            <TimelineSeparator>
-              <TimelineDot />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>시작</TimelineContent>
-          </TimelineItem>
-        )}
+        {validTimes.map((k) => (
+          <TimeItem time={time} k={k} />
+        ))}
       </Timeline>
     </React.Fragment>
   );
