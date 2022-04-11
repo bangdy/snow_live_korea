85cecd4a79690d096e1f302416e7e22a6937eff5
uberguri <bangdy14@gmail.com>
2022-04-11 23:14:58 +0900

    about (?) Button Elevation + 위치 모바일 여부에 따라 변경 추가

--- src/page/main.js
+++ src/page/main.js
@@ -6,9 +6,11 @@ import IconButton from "@mui/material/IconButton";
 import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 import Box from "@mui/material/Box";
 import { useNavigate } from "react-router-dom";
+import Paper from "@mui/material/Paper";
 
 const Main = (props) => {
   const resorts = useSelector((state) => state.resorts);
+  const isMobile = useSelector((state) => state.user.isMobile);
   const navigate = useNavigate();
 
   return (
@@ -22,24 +24,24 @@ const Main = (props) => {
       <Box
         pr={2}
         sx={{
-          position: "sticky",
+          position: isMobile ? "sticky" : "fixed",
+          right: !isMobile && 100,
           bottom: 30,
-          left: 100,
-
           alignSelf: "flex-end",
         }}>
-        <IconButton
-          onClick={() => navigate("/about")}
-          elevation={2}
-          sx={{
-            border: 1,
-            backgroundColor: "#f9f9f9",
-          }}
-          color="primary"
-          aria-label="upload picture"
-          component="span">
-          <QuestionMarkIcon sx={{ width: 20, height: 20 }} />
-        </IconButton>
+        <Paper elevation={3} sx={{ borderRadius: "50%" }}>
+          <IconButton
+            onClick={() => navigate("/about")}
+            sx={{
+              border: 1,
+              backgroundColor: "#f9f9f9",
+            }}
+            color="primary"
+            aria-label="upload picture"
+            component="span">
+            <QuestionMarkIcon sx={{ width: 20, height: 20 }} />
+          </IconButton>
+        </Paper>
       </Box>
     </Stack>
   );
