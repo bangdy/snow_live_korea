870268de96049a5ebd0dcbd8ffba36b3f4e3ca5b
uberguri <bangdy14@gmail.com>
2022-04-18 23:45:10 +0900

    Update BarChart.js

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -8,9 +8,11 @@ import { ResponsiveBar } from "@nivo/bar";
 const BarChart = ({ data /* see data tab */ }) => (
   <ResponsiveBar
     data={data}
-    keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
-    indexBy="country"
-    margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+    keys={["rating"]}
+    minValue={0}
+    maxValue={5}
+    indexBy="date"
+    margin={{ top: 50, right: 50, bottom: 50, left: 60 }}
     padding={0.4}
     valueScale={{ type: "linear" }}
     indexScale={{ type: "band", round: true }}
@@ -59,7 +61,7 @@ const BarChart = ({ data /* see data tab */ }) => (
       tickSize: 5,
       tickPadding: 5,
       tickRotation: 0,
-      legend: "country",
+      legend: "dates",
       legendPosition: "middle",
       legendOffset: 32,
     }}
@@ -67,7 +69,7 @@ const BarChart = ({ data /* see data tab */ }) => (
       tickSize: 5,
       tickPadding: 5,
       tickRotation: 0,
-      legend: "food",
+      legend: "rating",
       legendPosition: "middle",
       legendOffset: -40,
     }}
@@ -77,30 +79,6 @@ const BarChart = ({ data /* see data tab */ }) => (
       from: "color",
       modifiers: [["darker", 1.6]],
     }}
-    legends={[
-      {
-        dataFrom: "keys",
-        anchor: "bottom-right",
-        direction: "column",
-        justify: false,
-        translateX: 120,
-        translateY: 0,
-        itemsSpacing: 2,
-        itemWidth: 100,
-        itemHeight: 20,
-        itemDirection: "left-to-right",
-        itemOpacity: 0.85,
-        symbolSize: 20,
-        effects: [
-          {
-            on: "hover",
-            style: {
-              itemOpacity: 1,
-            },
-          },
-        ],
-      },
-    ]}
     role="application"
     ariaLabel="Nivo bar chart demo"
     barAriaLabel={function (e) {
