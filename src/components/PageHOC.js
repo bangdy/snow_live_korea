4792abe456d3c1979311cd39346254f8a09106a9
uberguri <bangdy14@gmail.com>
2022-05-14 10:33:09 +0900

    PageHOC 현재 path 에서만 guard 로직 적용

--- src/components/PageHOC.js
+++ src/components/PageHOC.js
@@ -1,43 +1,55 @@
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate, useLocation } from "react-router-dom";
 import { NavActionsContext } from "help/customHooks";
+
+import { nameToPath } from "help/util";
 
 const PageHOC = ({ Component, name }) => {
   const user = useSelector((state) => state.user);
   const { setActions } = useContext(NavActionsContext);
   const navigate = useNavigate();
   const location = useLocation();
+  const [loadingFin, setLoadingFin] = useState(false);
 
   //Logical Indicator
+  const preCheckingFin = user.uid === false || user.uid;
   const isLogin = user.uid;
-  const notProfileYet = isLogin && !(user.uid && user.profile);
-
-  useEffect(() => {
-    switch (name) {
-      case "Main":
-        notProfileYet && navigate("/my_page");
-        break;
-
-      case "Login":
-        notProfileYet && navigate("/my_page");
-        !notProfileYet && isLogin && navigate("/");
-        break;
-
-      case "MyPage":
-        !isLogin && navigate("/login");
-        break;
-
-      default:
-        break;
-    }
-  }, [location, isLogin]);
+  const notProfileYet = user.uid && !user.profile;
+  const isCurrentPath = window.location.pathname === nameToPath(name);
 
   useEffect(() => {
     setActions([]);
-  }, [location, isLogin]);
+    setLoadingFin(true);
+  }, [location, preCheckingFin]);
 
-  return Component;
+  useEffect(() => {
+    if (loadingFin && preCheckingFin && isCurrentPath) {
+      switch (name) {
+        case "Main":
+          notProfileYet && navigate("/my_page");
+          break;
+
+        case "Login":
+          notProfileYet && navigate("/my_page");
+          isLogin && navigate("/");
+          break;
+
+        case "MyPage":
+          !isLogin && navigate("/login");
+          break;
+
+        default:
+          break;
+      }
+    }
+  }, [location, preCheckingFin]);
+
+  if (loadingFin && preCheckingFin) {
+    return Component;
+  } else {
+    return <></>;
+  }
 };
 
 export default PageHOC;
